947908ec9622d33f004fb2b4b47f152a0adf67d6
ArienL704 <a.lettinga07@gmail.com>
2025-03-13 09:34:03 +0100

    MuziekBieb

---
+++ Python/muziekbieb.py
@@ -0,0 +1,22 @@
+import json
+
+def get_inputs():
+    name = input("Naam:")
+    d =  {}
+    d['yaar'] = input('yaar:')
+    d['auteur'] = input("auteur:")
+    return(name,d)
+
+out = {}
+
+while True:
+    exit = input('Will je nog wat toevoegen (y/n)? ')
+    if exit.lower() == 'n':
+        break
+    else:
+        name, d = get_inputs()
+        out[name] = d
+
+with open('Muziek.json','w') as f:
+    json.dump(out, f, indent=2)
+
