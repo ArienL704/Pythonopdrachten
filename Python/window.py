e0df820d43318ffc0bcd42dd080aff0144101f15
ArienL704 <a.lettinga07@gmail.com>
2025-03-13 11:33:11 +0100

    Window update

--- Python/window.py
+++ Python/window.py
@@ -3,8 +3,11 @@ import tkinter as tk
 window = tk.Tk()
 window.title("Hallo")
 
+window.geometry("200x200")
+window.minsize(200, 200)
+
 Label_hallo = tk.Label(text="Hello")
-Label_hallo.pack()
+Label_hallo.pack(pady = 20)
     
 
 goodbye = tk.Button(window, text="Goodbye", command=window.destroy)
