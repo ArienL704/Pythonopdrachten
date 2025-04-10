072d02142e436e65128773deeefe7d9155a91fb3
ArienL704 <a.lettinga07@gmail.com>
2025-04-10 10:27:49 +0200

    Klassen JSON

--- Python/Libary/Klassen.py
+++ Python/Libary/Klassen.py
@@ -1,7 +1,14 @@
+import json
+
 class Lokaal:
-  def __init__(self, naam, richting):
-    self.naam = naam
-    self.richting = richting
+    def __init__(self, naam, richting):
+        self.naam = naam
+        self.richting = richting
+
+    def to_dict(self):
+        return {"naam": self.naam, "richting": self.richting}
+
+
 
 L1 = Lokaal("C1.06", "West")
 L2 = Lokaal("C1.04", "West")
@@ -13,3 +20,9 @@ print(L2.naam)
 print(L2.richting)
 print(L3.naam)
 print(L3.richting)
+
+lokalen = [L1.to_dict(), L2.to_dict(), L3.to_dict()]
+
+# JSON file
+with open("lokalen.json", "w") as f:
+    json.dump(lokalen, f, indent=2)
