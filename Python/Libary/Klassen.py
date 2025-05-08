9b18492ba7f3c30e03127ffb05c64e92b5e743ce
ArienL704 <a.lettinga07@gmail.com>
2025-05-08 11:24:06 +0200

    Klassen tkinter

--- Python/Libary/Klassen.py
+++ Python/Libary/Klassen.py
@@ -1,3 +1,4 @@
+import tkinter as tk
 import json
 
 class Lokaal:
@@ -26,3 +27,27 @@ lokalen = [L1.to_dict(), L2.to_dict(), L3.to_dict()]
 # JSON file
 with open("lokalen.json", "w") as f:
     json.dump(lokalen, f, indent=2)
+
+    
+root = tk.Tk()
+
+canvas = tk.Canvas(root, height=700, width=700)
+
+canvas.pack()
+
+# Lokaal 1
+canvas.create_rectangle(50, 50, 100, 100, fill='blue')
+canvas.create_text(75, 75, text=L1.naam, fill='white')
+
+# Lokaal 2
+canvas.create_rectangle(175, 50, 225, 100, fill='blue')
+canvas.create_text(200, 75, text=L2.naam, fill='white')
+
+# Lokaal 3
+canvas.create_rectangle(300, 50, 350, 100, fill='blue')
+canvas.create_text(325, 75, text=L3.naam, fill='white')
+
+canvas.create_line(50, 120, 350, 120, fill='red', width=1)
+
+
+root.mainloop()
